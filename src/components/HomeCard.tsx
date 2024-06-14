5132d59c34d648937fb7caeeaee4f53e2880f584
Felipe da Silva Martins <femartins2010@gmail.com>
2024-06-14 11:52:52 -0300

    style: adicionando placeholder para o card da home

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import placeholder from '../assets/images/placeholder_image.png'
 
 export type CardItems = {
   titulo?: string
   image?: string
   id: number
   type?: 'person' | 'tvAndMovie'
+  link: string
 }
 
-const HomeCard = ({ titulo, image, type = 'tvAndMovie', id }: CardItems) => {
+const HomeCard = ({
+  titulo,
+  image,
+  type = 'tvAndMovie',
+  id,
+  link
+}: CardItems) => {
   const [img, setImg] = useState('')
 
   useEffect(() => {
@@ -16,26 +24,14 @@ const HomeCard = ({ titulo, image, type = 'tvAndMovie', id }: CardItems) => {
       fetch(`https://image.tmdb.org/t/p/w500${image}`).then((resp) =>
         setImg(resp.url)
       )
+    } else {
+      setImg(placeholder)
     }
   }, [image])
 
-  // const formatSinopse = (text: string) => {
-  //   if (text.length > 146) {
-  //     return text.slice(0, 143).concat('...')
-  //   }
-  //   return text
-  // }
-
-  // const formatTitle = (title: string) => {
-  //   if (title.length > 13) {
-  //     return title.slice(0, 13).concat('...')
-  //   }
-  //   return title
-  // }
-
   return (
     <Link
-      to={`/infos/${id}`}
+      to={`/${link}/${id}`}
       className="relative max-w-50 sm:h-auto sm:min-w-full relative select-none drop-shadow-xl hover:drop-shadow-2xl "
     >
       <img src={img} alt="" className="rounded-2xl  w-full" />
@@ -46,38 +42,6 @@ const HomeCard = ({ titulo, image, type = 'tvAndMovie', id }: CardItems) => {
       ) : (
         <></>
       )}
-
-      {/* Overlay do hover
-      <div
-        className={`hidden opacity-0 hover:opacity-100 transition easy-out duration-300 overflow-hidden absolute top-0 left-0 flex-col justify-between py-4 h-full w-full bg-hover-card-black rounded-2xl px-6 md:flex`}
-      >
-        <div>
-          <h2 className="font-display font-bold text-white text-center text-xl pb-4 2xl:text-xl">
-            {titulo ? formatTitle(titulo) : ''}
-          </h2>
-          <div
-            className={`flex justify-between items-center ${critica ? 'block' : 'hidden'}`}
-          >
-            <span className="font-display text-white md:hidden 2xl:block">
-              {`${critica ? 'Popularidade' : ''}`}
-            </span>
-            <span className="font-display text-white border rounded-full p-2.5 md:hidden 2xl:block ">
-              {critica ? critica.toFixed(1) : ''}
-            </span>
-          </div>
-        </div>
-        <div>
-          <h3 className="font-display text-white text-lg mb-4 md:hidden 2xl:block">
-            {`${sinopse ? 'sinopse' : ''}`}
-          </h3>
-          <p className="font-display text-white md:hidden 2xl:block text-xs">
-            {sinopse ? formatSinopse(sinopse) : ''}
-          </p>
-          <a className="text-white rounded-3xl bg-cyan-600 px-4 py-2 block w-full text-center mt-4 cursor-pointer">
-            Saiba mais
-          </a>
-        </div>
-      </div> */}
     </Link>
   )
 }
